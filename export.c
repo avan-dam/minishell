81814514dabdfafbba3234cc22ed294b75dd4b20
Amber Van dam <ambervandam@Ambers-MacBook-Pro.local>
2020-12-06 15:50:21 +0100

    sorry was working in master branch by mistake

--- export.c
+++ export.c
@@ -6,7 +6,7 @@
 /*   By: ambervandam <ambervandam@student.codam.      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/04 18:02:26 by ambervandam   #+#    #+#                 */
-/*   Updated: 2020/12/06 14:29:19 by avan-dam      ########   odam.nl         */
+/*   Updated: 2020/12/06 15:46:19 by ambervandam   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,27 @@ int    ft_export(t_mini *mini)
         return (0);
     var1 = ft_substrr(mini->more, 0, i);
     var2 = ft_substrr(mini->more, i + 1, ft_len(mini->more) - i - 1);
+    // check if var1 already in tlist and change var2 accordingly then
     adding = ft_lstnew(var1, var2);
     ft_lstadd_back(&mini->tlist, adding);
     ft_lstprint(mini->tlist);
     return (1);
 }
-static char	ft_find_and_replace_env(t_mini *mini, int i)
+
+static char *ft_replace_env(t_mini *mini, char *envvar)
+{
+	if (lst == NULL)
+		return ;
+	while (lst != NULL)
+	{
+        if (lst->var1 == envvar)
+            return (lst->var2);
+        lst = lst->next;
+	}
+    return (NULL);
+}
+
+static void	ft_find_env(t_mini *mini, int i)
 {
 	int		j;
 	char	*envvar
@@ -40,7 +55,8 @@ static char	ft_find_and_replace_env(t_mini *mini, int i)
 	while (mini->more[i] != '\0' && ' ')
 		i++;
     envvar = ft_substrr(mini->more, j, i - j);
-	return (envvar);
+	if (ft_replace_env(mini, envvar) != NULL)
+        
 }
 
 int		ft_check_env(t_mini *mini)
@@ -48,10 +64,10 @@ int		ft_check_env(t_mini *mini)
 	int i;
 
 	if mini->more[0] == '$'
-		ft_find_and_replace_env(mini, 0);
+		ft_find_env(mini, 0);
 	while (mini->more[i] != '\0')
 	{
 		if (mini->more[i] == '$')
-			ft_find_and_replace_env(mini, i);
+			ft_find_env(mini, i);
 	}
 }

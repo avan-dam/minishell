3c692b2ef9c8f02da1108ea2192c2061a7d1f005
Amber Van dam <Amber@Ambers-MacBook-Air-2.local>
2021-01-24 19:06:26 +0100

    some more 0 stuff

--- echo.c
+++ echo.c
@@ -6,7 +6,7 @@
 /*   By: ambervandam <ambervandam@student.codam.      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/04 11:40:20 by ambervandam   #+#    #+#                 */
-/*   Updated: 2021/01/14 11:56:51 by ambervandam   ########   odam.nl         */
+/*   Updated: 2021/01/24 13:29:21 by Amber         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,7 @@ static int    ft_echo_n(char *string, t_mini *mini)
 	}
     string = ft_strtrim(string, " ");
     ft_putstr_fd(string, mini->stdout);
+    mini->exit = 0;
     return(0);
 }
 
@@ -81,18 +82,67 @@ static int  ft_check_empty(char *string)
     return (0);
 }
 
+static char		*line_replacedd(char *start, char *newvar, char *end)
+{
+	char	*temp;
+	char	*newline;
+
+	temp = ft_strjoin(start, newvar);
+	newline = ft_strjoin(temp, end);
+	return (newline);
+}
+
+char    *ft_check_exit_status(char *string, t_mini *mini)
+{
+    int i;
+    char *start;
+    char *end;
+    char *middle;
+
+    i = 0;
+    if (string == NULL)
+        return(NULL);
+    if (ft_strlen(string) < 2)
+        return (NULL);
+    while (string[i + 1] != '\0')
+    {
+        printf("in while\n");
+        if (string[i] == '$' && string[i + 1] == '?')
+        {
+            printf("in condition\n");
+			ft_memmove(&string[i], &string[i+1], ft_strlen(string) - 1);
+			i--;
+            printf("1string is not %s\n", string);
+			ft_memmove(&string[i], &string[i+1], ft_strlen(string) - 1);
+			i--;
+            printf("2string is not %s\n", string);
+	        start = ft_substr(string, 0, i + 1);
+            middle = ft_itoa(mini->exit);
+	        end = ft_substr(string, i, ft_strlen(string) - i);
+            string = line_replacedd(start, middle, end);
+            printf("1string is not %s\n", string);
+        }
+        i++;
+    }
+    return (string);
+}
+
 int    ft_echo(char *string, t_mini *mini)
 {
     if (string == NULL)
         return(ft_putchar('\n'));
     if (ft_check_empty(string) == 1)
         return(ft_putchar_fd('\n', mini->stdout));
+    if (ft_check_exit_status(string, mini) != NULL)
+        string = ft_check_exit_status(string, mini);
     else if ((string[0] == '-') && (string[1] == 'n') && ((string[2] == ' ') || (string[2] == '\0') || (string[2] == 'n')))
     {
         if (ft_echo_n(string, mini) != 2)
             return(0);
     }
     string = ft_strtrim(string, " ");
+    printf("string: [%s]\n", string);
     ft_putstr_fd(string, mini->stdout);
+    mini->exit = 0;
     return(ft_putchar_fd('\n', mini->stdout));
 }
--- exit_status.c
+++ exit_status.c
@@ -6,7 +6,7 @@
 /*   By: Amber <Amber@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/24 10:48:47 by Amber         #+#    #+#                 */
-/*   Updated: 2021/01/24 11:02:37 by Amber         ########   odam.nl         */
+/*   Updated: 2021/01/24 12:49:07 by Amber         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,5 @@ void		ft_printf_exit_status(t_mini *mini)
 	exitstatus = ft_itoa(mini->exit);
 	ft_putstr_fd(exitstatus, mini->stdout);
 	ft_putstr_fd("\n", mini->stdout);
+	mini->exit = 127;
 }

4133b243334e13c649fcdd901109472dade7be59
Amber Van dam <ambervandam@Ambers-MacBook-Pro.local>
2021-02-11 10:43:26 +0100

    redir improved

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: salbregh <salbregh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/17 22:36:40 by salbregh      #+#    #+#                 */
-/*   Updated: 2021/02/10 18:18:13 by salbregh      ########   odam.nl         */
+/*   Updated: 2021/02/11 10:42:44 by ambervandam   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 

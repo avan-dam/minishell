fe5976aeaffd5193c8776ba965155d89d2515f3b
Sanne Albreghs <scalbreghs@gmail.com>
2021-02-23 13:43:48 +0000

    norm execve

--- execve/execve.c
+++ execve/execve.c
@@ -6,35 +6,35 @@
 /*   By: salbregh <salbregh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/27 16:41:50 by salbregh      #+#    #+#                 */
-/*   Updated: 2021/02/17 17:49:04 by salbregh      ########   odam.nl         */
+/*   Updated: 2021/02/23 13:41:27 by salbregh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static void		exec_builtin(t_base *tmp, t_mini *mini)
+static void	exec_builtin(t_base *tmp, t_mini *mini)
 {
 	if (ft_strcmp(tmp->argv[0], "$?") == 0)
 		ft_printf_exit_status(mini);
-	else if (ft_strcmp(tmp->argv[0], "env") == 0 ||
-	ft_strcmp(tmp->argv[0], "/usr/bin/env") == 0)
+	else if (ft_strcmp(tmp->argv[0], "env") == 0
+		|| ft_strcmp(tmp->argv[0], "/usr/bin/env") == 0)
 		ft_lstprint(mini->env1, mini, 0);
 	else if (ft_strcmp(tmp->argv[0], "export") == 0)
 		ft_export(tmp, mini);
-	else if (ft_strcmp(tmp->argv[0], "echo") == 0 ||
-	ft_strcmp(tmp->argv[0], "/bin/echo") == 0)
+	else if (ft_strcmp(tmp->argv[0], "echo") == 0
+		|| ft_strcmp(tmp->argv[0], "/bin/echo") == 0)
 		ft_echo(tmp, mini);
-	else if ((ft_strcmp(tmp->argv[0], "pwd") == 0) ||
-	(ft_strcmp(tmp->argv[0], "/bin/pwd") == 0))
+	else if ((ft_strcmp(tmp->argv[0], "pwd") == 0)
+		|| (ft_strcmp(tmp->argv[0], "/bin/pwd") == 0))
 		ft_pwd(mini);
-	else if (ft_strcmp(tmp->argv[0], "cd") == 0 ||
-	ft_strcmp(tmp->argv[0], "/usr/bin/cd") == 0)
+	else if (ft_strcmp(tmp->argv[0], "cd") == 0
+		|| ft_strcmp(tmp->argv[0], "/usr/bin/cd") == 0)
 		ft_cd(tmp, mini);
 	else if (ft_strcmp(tmp->argv[0], "unset") == 0)
 		ft_unset(mini, tmp->argv[1]);
 }
 
-static void		parent_proces(pid_t pid, t_mini *mini, t_base *ptr, int piped)
+static void	parent_proces(pid_t pid, t_mini *mini, t_base *ptr, int piped)
 {
 	int			status;
 
@@ -50,7 +50,7 @@ static void		parent_proces(pid_t pid, t_mini *mini, t_base *ptr, int piped)
 		close(ptr->prev->fd[0]);
 }
 
-static void		execve_commands(t_base *ptr, char **envp, t_mini *mini)
+static void	execve_commands(t_base *ptr, char **envp, t_mini *mini)
 {
 	pid_t		pid;
 	int			piped;
@@ -62,22 +62,23 @@ static void		execve_commands(t_base *ptr, char **envp, t_mini *mini)
 		if (pipe(ptr->fd))
 			exit(0);
 	}
-	if ((pid = fork()) < 0)
+	pid = fork();
+	if (pid < 0)
 		exit(0);
 	if (pid == 0)
 	{
 		dup2(mini->stdin, STDIN);
 		dup2(mini->stdout, STDOUT);
-		if (ft_is_builtin_command(ptr->argv[0]) == 0 &&
-		look_for_non_builtin(ptr) == 2)
+		if (ft_is_builtin_command(ptr->argv[0]) == 0
+			&& look_for_non_builtin(ptr) == 2)
 			unvalid_ident(ptr->argv[0], mini, 127);
 		if (ptr->type == TYPE_PIPE && dup2(ptr->fd[1], STDOUT) < 0)
 			exit(EXIT_FAILURE);
 		if (ptr->prev && ptr->prev->type == TYPE_PIPE
-		&& dup2(ptr->prev->fd[0], STDIN) < 0)
+			&& dup2(ptr->prev->fd[0], STDIN) < 0)
 			exit(EXIT_FAILURE);
-		if (ft_strcmp(ptr->argv[0], "exit") != 0 &&
-		ft_is_builtin_command(ptr->argv[0]) == 1)
+		if (ft_strcmp(ptr->argv[0], "exit") != 0
+			&& ft_is_builtin_command(ptr->argv[0]) == 1)
 			exec_builtin(ptr, mini);
 		else if (execve(ptr->argv[0], ptr->argv, envp) < 0 || !ptr->argv[1])
 			exit(EXIT_FAILURE);
@@ -89,7 +90,7 @@ static void		execve_commands(t_base *ptr, char **envp, t_mini *mini)
 		parent_proces(pid, mini, ptr, piped);
 }
 
-int				exec_cmds(t_base *tmp, char **envp, t_mini *mini)
+int	exec_cmds(t_base *tmp, char **envp, t_mini *mini)
 {
 	while (tmp)
 	{
@@ -105,14 +106,14 @@ int				exec_cmds(t_base *tmp, char **envp, t_mini *mini)
 				return (0);
 			tmp = ft_redir(mini, tmp);
 		}
-		if ((tmp->type == TYPE_PIPE ||
-		(tmp->prev && tmp->prev->type == TYPE_PIPE)) &&
-		ft_is_builtin_command(tmp->argv[0]) == 1)
+		if ((tmp->type == TYPE_PIPE
+				|| (tmp->prev && tmp->prev->type == TYPE_PIPE))
+			&& ft_is_builtin_command(tmp->argv[0]) == 1)
 			execve_commands(tmp, envp, mini);
 		else if (ft_strcmp("", tmp->argv[0]) == 0)
 			break ;
-		else if (ft_strcmp(tmp->argv[0], "exit") != 0 &&
-		ft_is_builtin_command(tmp->argv[0]) == 1)
+		else if (ft_strcmp(tmp->argv[0], "exit") != 0
+			&& ft_is_builtin_command(tmp->argv[0]) == 1)
 			exec_builtin(tmp, mini);
 		else if (ft_strcmp(tmp->argv[0], "exit") == 0)
 		{
--- execve/non_builtins.c
+++ execve/non_builtins.c
@@ -6,24 +6,26 @@
 /*   By: salbregh <salbregh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/28 15:06:53 by salbregh      #+#    #+#                 */
-/*   Updated: 2021/02/11 14:07:46 by salbregh      ########   odam.nl         */
+/*   Updated: 2021/02/23 13:39:19 by salbregh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 #include <dirent.h>
 
-static int		ft_check_in_usr_bin(t_base *ptr, struct dirent *dit, DIR *dirp)
+static int	ft_check_in_usr_bin(t_base *ptr, struct dirent *dit, DIR *dirp)
 {
 	char		*command;
 	char		*tmp;
+	int			usrbin;
 
 	command = ptr->argv[0];
 	tmp = command;
-	if (ft_strcmp(ft_substr(tmp, 0, 9), "/usr/bin/") == 0)
+	usrbin = ft_strcmp(ft_substr(tmp, 0, 9), "/usr/bin/");
+	if (usrbin == 0)
 		tmp = ft_substr(command, 9, ft_strlen(command) - 9);
 	dit = readdir(dirp);
-	while ((dit = readdir(dirp)) != NULL)
+	while (dit)
 	{
 		if (ft_strcmp(dit->d_name, tmp) == 0)
 		{
@@ -31,20 +33,24 @@ static int		ft_check_in_usr_bin(t_base *ptr, struct dirent *dit, DIR *dirp)
 			closedir(dirp);
 			return (0);
 		}
+		dit = readdir(dirp);
 	}
 	return (1);
 }
 
-static int		ft_check_in_bin(t_base *ptr, struct dirent *dit, DIR *dirp)
+static int	ft_check_in_bin(t_base *ptr, struct dirent *dit, DIR *dirp)
 {
 	char		*command;
 	char		*tmp;
+	int			bin;
 
 	command = ptr->argv[0];
 	tmp = command;
-	if (ft_strcmp(ft_substr(tmp, 0, 5), "/bin/") == 0)
+	bin = ft_strcmp(ft_substr(tmp, 0, 5), "/bin/");
+	if (bin == 0)
 		tmp = ft_substr(command, 5, ft_strlen(command) - 5);
-	while ((dit = readdir(dirp)) != NULL)
+	dit = readdir(dirp);
+	while (dit)
 	{
 		if (ft_strcmp(dit->d_name, tmp) == 0)
 		{
@@ -52,20 +58,21 @@ static int		ft_check_in_bin(t_base *ptr, struct dirent *dit, DIR *dirp)
 			closedir(dirp);
 			return (0);
 		}
+		dit = readdir(dirp);
 	}
 	return (1);
 }
 
-int		ft_is_builtin_command(char *str)
+int	ft_is_builtin_command(char *str)
 {
-	if ((ft_strcmp(str, "echo") == 0 || ft_strcmp(str, "/bin/echo") == 0 ||
-		ft_strcmp(str, "cd") == 0) || ft_strcmp(str, "/usr/bin/cd") == 0 ||
-		ft_strcmp(str, "env") == 0 || ft_strcmp(str, "/usr/bin/env") == 0 ||
-		ft_strcmp(str, "exit") == 0 ||
-		ft_strcmp(str, "export") == 0 ||
-		ft_strcmp(str, "pwd") == 0 || ft_strcmp(str, "/bin/pwd") == 0 ||
-		ft_strcmp(str, "unset") == 0 ||
-		ft_strcmp(str, "$?") == 0)
+	if (ft_strcmp(str, "echo") == 0 || ft_strcmp(str, "/bin/echo") == 0
+		|| ft_strcmp(str, "cd") == 0 || ft_strcmp(str, "/usr/bin/cd") == 0
+		|| ft_strcmp(str, "env") == 0 || ft_strcmp(str, "/usr/bin/env") == 0
+		|| ft_strcmp(str, "exit") == 0
+		|| ft_strcmp(str, "export") == 0
+		|| ft_strcmp(str, "pwd") == 0 || ft_strcmp(str, "/bin/pwd") == 0
+		|| ft_strcmp(str, "unset") == 0
+		|| ft_strcmp(str, "$?") == 0)
 		return (1);
 	return (0);
 }
@@ -75,7 +82,7 @@ int		ft_is_builtin_command(char *str)
 ** 	opendir() opes the directory and point to the first entry in the directory
 */
 
-int				look_for_non_builtin(t_base *ptr)
+int	look_for_non_builtin(t_base *ptr)
 {
 	t_base			*tmp;
 	DIR				*dirp;
@@ -85,12 +92,14 @@ int				look_for_non_builtin(t_base *ptr)
 	if (ft_is_builtin_command(tmp->argv[0]) == 1)
 		return (1);
 	dit = NULL;
-	if ((dirp = opendir("/bin")) == NULL)
+	dirp = opendir("/bin");
+	if (dirp == NULL)
 		exit(0);
 	if (ft_check_in_bin(tmp, dit, dirp) == 0)
 		return (0);
 	closedir(dirp);
-	if ((dirp = opendir("/usr/bin")) == NULL)
+	dirp = opendir("/usr/bin");
+	if (dirp == NULL)
 		exit(0);
 	if (ft_check_in_usr_bin(ptr, dit, dirp) == 0)
 		return (0);

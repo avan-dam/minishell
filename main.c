edf009f0f0fd3f2846b42f1c455a918e7d1ee883
Sanne Albreghs <scalbreghs@gmail.com>
2021-03-07 14:17:18 +0100

    merge master

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: salbregh <salbregh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/17 22:36:40 by salbregh      #+#    #+#                 */
-/*   Updated: 2021/03/06 18:11:58 by salbregh      ########   odam.nl         */
+/*   Updated: 2021/03/07 14:05:31 by salbregh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 

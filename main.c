9262081e45d3c0f4a41a2974c2a7b7cdcb5e0e3a
Amber Van dam <avan-dam@f1r5s15.codam.nl>
2021-03-16 15:15:53 +0100

    yes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: salbregh <salbregh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/17 22:36:40 by salbregh      #+#    #+#                 */
-/*   Updated: 2021/03/16 12:14:32 by avan-dam      ########   odam.nl         */
+/*   Updated: 2021/03/16 15:04:20 by avan-dam      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 

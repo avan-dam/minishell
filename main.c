83b9a711a0faa709c6c0041fc5338498122c8b98
Amber Van dam <avan-dam@f1r6s13.codam.nl>
2021-03-25 12:37:20 +0100

    sh

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: salbregh <salbregh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/17 22:36:40 by salbregh      #+#    #+#                 */
-/*   Updated: 2021/03/24 18:25:09 by ambervandam   ########   odam.nl         */
+/*   Updated: 2021/03/25 12:35:04 by avan-dam      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ static void	handle_line(int lineret, t_mini *mini, char **envp)
 			exit(1);
 		}
 		ft_send_to_parser(line, mini, envp);
+		// system("leaks minishell");
 	}
 	if (lineret == 0)
 		ft_signals(mini, 1);

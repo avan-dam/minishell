0f9ed8c625b2cf54c1a091b8e9d8d47800acffe3
Amber Van dam <ambervandam@Ambers-MacBook-Pro.local>
2021-04-01 18:14:20 +0200

    nothing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: salbregh <salbregh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/17 22:36:40 by salbregh      #+#    #+#                 */
-/*   Updated: 2021/03/31 17:57:12 by ambervandam   ########   odam.nl         */
+/*   Updated: 2021/04/01 18:14:13 by ambervandam   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 

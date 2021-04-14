6e1609d6b38a2e2a59967c259d61af30d7e05d07
Amber Van dam <ambervandam@ambers-mbp.home>
2021-04-14 11:13:39 +0100

    getting there

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: salbregh <salbregh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/29 23:26:56 by salbregh      #+#    #+#                 */
-/*   Updated: 2021/04/14 11:11:14 by ambervandam   ########   odam.nl         */
+/*   Updated: 2021/04/14 11:13:24 by ambervandam   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@
 # include <fcntl.h>
 # include <signal.h>
 # include <dirent.h>
-
-#include <stdio.h> //out
 
 # define R O_RDWR
 # define C O_CREAT
